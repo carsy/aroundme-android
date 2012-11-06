dc098d191cd51d6f0060fe5efd24e068ec0bc2c6
carsy <zebateira@acer-aspire-5220.(none)>
2012-11-06 16:22:22 +0000

    init

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AroundMe::Application.config.secret_token = 'd3cc5299aa385e7c49a4e83c67bbdde624a9443ad9650387c36d946fba725797411a4b601a00dcc1aa430f6ea808883a8560ea17c11768a0178c4879d7979c43'
